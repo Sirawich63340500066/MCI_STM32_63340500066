9a6c26b39b9b185efd11e3a9acc1e582835850f5
Sirawich63340500066 <sirawich.kmutt@mail.kmutt.ac.th>
2022-02-27 01:10:08 +0700

    Test

--- TEST_63340500066/.settings/Core/Src/main.c
+++ TEST_63340500066/.settings/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+/*Hello
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
